14eedf0ce99449defd6c70dd24d817e536d6c957
Horváth Ádám <jackadam852@gmail.com>
2023-07-24 20:39:25 +0200

    adding and removing from playlist

--- lib/model/playlist.dart
+++ lib/model/playlist.dart
@@ -1,8 +1,12 @@
+import 'package:yt_playlists_plus/model/client.dart';
+import 'package:yt_playlists_plus/model/video.dart';
+
 class Playlist {
   String id;
   String title;
   String author;
   String thumbnailUrl;
+  List<Video> videos = [];
 
   Playlist({
     required this.id,
@@ -10,6 +14,29 @@ class Playlist {
     required this.author,
     required this.thumbnailUrl,
   });
+
+  bool tryAddVideo(Video video) {
+    if (!videos.contains(video)) {
+      videos.add(video);
+      return true;
+    }
+    return false;
+  }
+
+  bool tryRemoveVideo(Video video) {
+    if (videos.contains(video)) {
+      videos.remove(video);
+      return true;
+    }
+    return false;
+  }
+
+  Stream<Video> getVideos(String playlistId, YoutubeClient client) async* {
+    await for (Video video in client.getVideosFromPlaylist(id)) {
+      tryAddVideo(video);
+      yield video;
+    }
+  }
 
   @override
   String toString() {
