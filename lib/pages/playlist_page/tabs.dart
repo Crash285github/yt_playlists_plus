32d9b64ef9346d7b13ccb37a1587107b18543932
Crash285 <jackadam852@gmail.com>
2023-08-06 00:31:10 +0200

    added slidingpanel dependency

--- lib/pages/playlist_page/tabs.dart
+++ lib/pages/playlist_page/tabs.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:yt_playlists_plus/model/playlist/playlist_status.dart';
-import 'package:yt_playlists_plus/pages/playlist_page/planned_list.dart';
 import 'package:yt_playlists_plus/widgets/history_widget.dart';
-import 'package:yt_playlists_plus/widgets/planned_widget.dart';
 import 'package:yt_playlists_plus/widgets/video_widget.dart';
 import '../../model/playlist/playlist.dart';
 import '../../model/video/video.dart';
