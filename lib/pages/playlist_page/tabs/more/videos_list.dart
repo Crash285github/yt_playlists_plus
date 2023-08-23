7aabb233f30fd0224524dcc53fbad1b0c536b106
Crash285 <jackadam852@gmail.com>
2023-08-23 18:56:44 +0200

    android planned height fix

--- lib/pages/playlist_page/tabs/more/videos_list.dart
+++ lib/pages/playlist_page/tabs/more/videos_list.dart
@@ -41,7 +41,7 @@ class _VideosListState extends State<VideosList>
                 isInteractable: false,
               ));
         }),
-        const BottomPadding(androidHeight: 230, windowsHeight: 210),
+        const BottomPadding(androidHeight: 250, windowsHeight: 210),
       ],
     );
   }
