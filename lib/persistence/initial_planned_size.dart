25d9fa2b2c4bb5d91726d1709d8b287a038be517
Crash285 <jackadam852@gmail.com>
2023-08-28 15:09:03 +0200

    planned size setting added to Persistence

---
+++ lib/persistence/initial_planned_size.dart
@@ -0,0 +1,11 @@
+enum InitialPlannedSize {
+  ///Default value
+  normal(displayName: "Default"),
+
+  ///Lowest setting
+  minimal(displayName: "Minimal");
+
+  const InitialPlannedSize({required this.displayName});
+
+  final String displayName;
+}
