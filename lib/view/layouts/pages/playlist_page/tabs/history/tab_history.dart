ad22c27d0d66bf641163bc22e558c3c069c10f34
Crash285 <jackadam852@gmail.com>
2023-10-04 17:48:05 +0200

    history top row removed

--- lib/view/layouts/pages/playlist_page/tabs/history/tab_history.dart
+++ lib/view/layouts/pages/playlist_page/tabs/history/tab_history.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:yt_playlists_plus/controller/playlists_controller.dart';
 import 'package:yt_playlists_plus/services/popup_service/open_confirm_dialog.dart';
 import 'package:yt_playlists_plus/services/popup_service/popup_service.dart';
 import 'package:yt_playlists_plus/view/layouts/pages/playlist_page/tabs/history/empty_history.dart';
 import 'package:yt_playlists_plus/view/layouts/pages/playlist_page/tabs/history/history_list.dart';
-import 'package:yt_playlists_plus/view/layouts/pages/playlist_page/tabs/history/history_top_row.dart';
 import 'package:yt_playlists_plus/model/video_history.dart';
-import 'package:yt_playlists_plus/controller/playlists_controller.dart';
 
 class HistoryTab extends StatefulWidget {
   final List<VideoHistory> history;
@@ -18,30 +17,38 @@ class HistoryTab extends StatefulWidget {
 class _HistoryTabState extends State<HistoryTab> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        HistoryTopRow(
-          onClearPressed: widget.history.isEmpty
-              ? null
-              : () async {
-                  PopUpService()
-                      .openConfirmDialog(
-                    context: context,
-                    title: "Clear history?",
-                  )
-                      .then((value) {
-                    if (value ?? false) {
-                      setState(() => widget.history.clear());
-                      PlaylistsController().save();
-                    }
-                  });
-                },
-        ),
-        const Divider(),
-        widget.history.isEmpty
-            ? const EmptyHistory()
-            : HistoryList(history: widget.history),
-      ],
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Column(
+        children: [
+          const SizedBox(height: 8),
+          widget.history.isEmpty
+              ? const EmptyHistory()
+              : HistoryList(history: widget.history),
+        ],
+      ),
+      floatingActionButton: widget.history.isEmpty
+          ? null
+          : FloatingActionButton(
+              onPressed: () async {
+                PopUpService()
+                    .openConfirmDialog(
+                  context: context,
+                  title: "Clear history?",
+                )
+                    .then((value) {
+                  if (value ?? false) {
+                    setState(() => widget.history.clear());
+                    PlaylistsController().save();
+                  }
+                });
+              },
+              tooltip: "Clear history",
+              child: const Icon(
+                Icons.delete_sweep_outlined,
+                size: 30,
+              ),
+            ),
     );
   }
 }
