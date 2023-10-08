62e9f9227c438da816313bce2f7410ad2e9eb968
Crash285 <91914424+Crash285github@users.noreply.github.com>
2023-10-08 12:20:56 +0200

    added indexes to videos (#179)

--- lib/view/templates/video/video_details.dart
+++ lib/view/templates/video/video_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:yt_playlists_plus/config.dart';
 import 'package:yt_playlists_plus/extensions/hide_topic.dart';
 import 'package:yt_playlists_plus/controller/video_controller.dart';
 import 'package:yt_playlists_plus/controller/settings_controllers/hide_topics_controller.dart';
@@ -19,25 +20,28 @@ class VideoDetails extends StatelessWidget {
     final ThemeData theme = Theme.of(context);
 
     return Flexible(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Tooltip(
-            message: video.title,
-            child: Text(
-              video.title,
-              overflow: TextOverflow.ellipsis,
-              style: theme.textTheme.titleMedium,
+      child: Padding(
+        padding: const EdgeInsets.only(left: AppConfig.spacing / 2),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Tooltip(
+              message: video.title,
+              child: Text(
+                video.title,
+                overflow: TextOverflow.ellipsis,
+                style: theme.textTheme.titleMedium,
+              ),
             ),
-          ),
-          const SizedBox(height: 7),
-          Text(
-            hideTopics ? video.author.hideTopic() : video.author,
-            overflow: TextOverflow.ellipsis,
-            style: theme.textTheme.titleSmall!
-                .copyWith(color: theme.colorScheme.onSurface.withOpacity(0.5)),
-          ),
-        ],
+            const SizedBox(height: 5),
+            Text(
+              hideTopics ? video.author.hideTopic() : video.author,
+              overflow: TextOverflow.ellipsis,
+              style: theme.textTheme.titleSmall!.copyWith(
+                  color: theme.colorScheme.onSurface.withOpacity(0.5)),
+            ),
+          ],
+        ),
       ),
     );
   }
